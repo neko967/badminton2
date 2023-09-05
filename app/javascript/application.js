decf1cbad6c4b43b7d6d76d0c5111c4f82c8f228
neko967 <113199838+neko967@users.noreply.github.com>
2023-09-05 17:16:09 +0900

    Add: componentフォルダを作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-
-const Hello = props => (
-  <div>こんにちは {props.name} さん！</div>
-)
-
-Hello.defaultProps = {
-  name: '名無し'
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('root');
-  createRoot(container).render(<Hello name='やまうちなお' />);
-})
+import "./components/01_App.js"
---
+++ app/javascript/components/01_App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+const Hello = props => (
+  <div>こんにちは {props.name} さん！</div>
+)
+
+Hello.defaultProps = {
+  name: '名無し'
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('root');
+  createRoot(container).render (
+    <Hello name='やまうちなお' />
+  );
+})
