b7c3a5ef748a37ad57e2fe1e38bc1e4d14669d31
neko967 <113199838+neko967@users.noreply.github.com>
2023-09-05 17:12:18 +0900

    Add: Reactをインストール

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,15 @@
-// Entry point for the build script in your package.json
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+const Hello = props => (
+  <div>こんにちは {props.name} さん！</div>
+)
+
+Hello.defaultProps = {
+  name: '名無し'
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('root');
+  createRoot(container).render(<Hello name='やまうちなお' />);
+})
